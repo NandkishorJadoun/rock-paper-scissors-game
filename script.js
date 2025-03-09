6b1427eb54212ddbc18e6ae829700fbe531be6dc
NandkishorJadoun <183695114+NandkishorJadoun@users.noreply.github.com>
2025-03-09 18:17:50 +0530

    refactor: add logic

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function getComputerChoice() {
-  let x = Math.floor(Math.random() * 100) + 1;
+  let x = Math.random();
 
-  if (x >= 1 && x < 33) {
+  if (x >= 0 && x < 0.33) {
     return "rock";
   }
-  else if (x >= 33 && x < 66) {
+  else if (x >= 0.33 && x < 0.66) {
     return "paper";
   }
   else {
@@ -14,124 +14,92 @@ function getComputerChoice() {
 
 function playGame() {
 
+  const compScore = document.querySelector("#computer-score")
+  const yourScore = document.querySelector("#your-score")
+  const roundResult = document.createElement("div")
+
   let humanScore = 0;
   let computerScore = 0;
-  
+
   const rock = document.querySelector("#rock");
   const paper = document.querySelector("#paper");
   const scissors = document.querySelector("#scissors");
 
-  const result = document.querySelector("#result");
-
-  const moves = document.createElement("div");
-  result.appendChild(moves);
-  const roundWinner = document.createElement("div");
-  result.appendChild(roundWinner);
-  let winner = document.createElement("div");
-  result.appendChild(winner);
-
-  const humanScoreDisplay = document.querySelector("#humanScore");
-  const computerScoreDisplay = document.querySelector("#computerScore")
-
   function playRound(humanChoice, computerChoice) {
 
-    humanChoice = humanChoice.toLowerCase();
-
     if (humanChoice == computerChoice) {
-     roundWinner.textContent = "Tie! Play again";
+      roundResult.textContent = "YOU DRAW"
     }
 
     else if (humanChoice == "rock" && computerChoice == "paper") {
-      roundWinner.textContent = "You lose! Paper beats Rock";
       computerScore++;
+      roundResult.textContent = "YOU LOSE"
     }
 
     else if (humanChoice == "rock" && computerChoice == "scissors") {
-      roundWinner.textContent = "You Won! Rock beats scissors";
       humanScore++;
+      roundResult.textContent = "YOU WON"
     }
 
     else if (humanChoice == "paper" && computerChoice == "rock") {
-      roundWinner.textContent = ("You won! Paper beats Rock");
       humanScore++;
+      roundResult.textContent = "YOU WON"
+
     }
 
     else if (humanChoice == "paper" && computerChoice == "scissors") {
-      roundWinner.textContent = ("You Lose! Scissors beats Paper");
       computerScore++;
+      roundResult.textContent = "YOU LOSE"
+
     }
 
     else if (humanChoice == "scissors" && computerChoice == "rock") {
-      roundWinner.textContent = ("You Lose! Rock beats scissors");
       computerScore++;
+      roundResult.textContent = "YOU LOSE"
+
     }
 
     else if (humanChoice == "scissors" && computerChoice == "paper") {
-      roundWinner.textContent = ("You Won! Scissors beats Paper");
       humanScore++;
-    }
-    humanScoreDisplay.textContent = `Human Score: ${humanScore}`;
-    computerScoreDisplay.textContent = `Computer Score: ${computerScore}`
-    
+      roundResult.textContent = "YOU WON"
 
-    if(humanScore == 5){
-      winner.textContent = `Human Wins by (${humanScore} - ${computerScore})`
-      rock.disabled = true;
-      paper.disabled = true;
-      scissors.disabled = true;
     }
 
-    else if(computerScore == 5){
-      winner.textContent = `Computer Wins by (${computerScore} - ${humanScore})`
-      rock.disabled = true;
-      paper.disabled = true;
-      scissors.disabled = true;
-    }
+    compScore.textContent = computerScore;
+    yourScore.textContent = humanScore;
   }
+  
+  const compChoice = document.createElement("div")
+  const yourChoice = document.createElement("div")
 
-  rock.addEventListener("click", ()=>{
-    let humanSelection = "rock";
+  rock.addEventListener("click", () => {
     let computerSelection = getComputerChoice();
-    moves.textContent = `
-    Human Move: ${humanSelection} and
-    Computer Move: ${computerSelection}
-    `
-    playRound(humanSelection, computerSelection);
+    playRound("rock", computerSelection);
+    yourChoice.textContent = `You choose ROCK`
+    compChoice.textContent = `Computer choose ${computerSelection.toUpperCase()}`
   })
 
-  paper.addEventListener("click", ()=>{
-    let humanSelection = "paper";
+  paper.addEventListener("click", () => {
     let computerSelection = getComputerChoice();
-    moves.textContent = `
-    Human Move: ${humanSelection} and
-    Computer Move: ${computerSelection}
-    `
-    playRound(humanSelection, computerSelection); 
+    playRound("paper", computerSelection);
+    yourChoice.textContent = `You choose PAPER`
+    compChoice.textContent = `Computer choose ${computerSelection.toUpperCase()}`
+
   })
 
-  scissors.addEventListener("click", ()=>{
-    let humanSelection = "scissors";
+  scissors.addEventListener("click", () => {
     let computerSelection = getComputerChoice();
-    moves.textContent = `
-    Human Move: ${humanSelection} and
-    Computer Move: ${computerSelection}
-    `
-    playRound(humanSelection, computerSelection);
+    playRound("scissors", computerSelection);
+    yourChoice.textContent = `You choose SCISSORS`
+    compChoice.textContent = `Computer choose ${computerSelection.toUpperCase()}`
+
   })
 
- const reset = document.querySelector("#reset");
- reset.addEventListener("click", ()=>{
-  humanScore = 0;
-  computerScore = 0;
-  humanScoreDisplay.textContent = `Human Score: ${humanScore}`;
-  computerScoreDisplay.textContent = `Computer Score: ${computerScore}`
-  roundWinner.textContent = '';
-  winner.textContent = "";
-  moves.textContent = "";
-  rock.disabled = false;
-  paper.disabled = false;
-  scissors.disabled = false;
- })
+  const choices = document.querySelector("#choices")
+  const result = document.querySelector("#result")
+  choices.appendChild(compChoice)
+  choices.appendChild(yourChoice)
+  result.appendChild(roundResult)
 
 }
 playGame();
